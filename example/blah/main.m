ed1531dcb584e586419ed7b7dbba4823e7cdff3a
Mark Frawley <markfrawley@gmail.com>
2015-03-02 22:19:26 +0100

    added example ios project

---
+++ example/blah/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  blah
+//
+//  Created by Mark Frawley on 02/03/2015.
+//  Copyright (c) 2015 Mark Frawley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
