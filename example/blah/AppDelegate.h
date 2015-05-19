c1fab9d10e5f869bd82b2e3685f00bde5a3a232e
Mark Frawley <markfrawley@gmail.com>
2015-05-19 23:35:37 +0200

    merge half

---
+++ example/blah/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blah
+//
+//  Created by Mark Frawley on 02/03/2015.
+//  Copyright (c) 2015 Mark Frawley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
